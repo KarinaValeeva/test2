5311adba99e3bfa3facb85226c2f23e033ae83f8
Karina <Karina@MacBook-Pro-Karina.local>
2017-02-18 20:33:24 +0300

    18.02.17
    Увеличила размер всех элементов, доделала первый ряд чашек c анимацией,
    нарисовала дизайн второго типа чашек

--- javascripts/click.js
+++ javascripts/click.js
@@ -9,3 +9,21 @@ $(function() {
     $(".second").show(".first", "fadeInDown")
   })
 })
+
+$(function() {
+  $(".cup1").click(function () {
+    $(".third").show(".third", "fadeInDown")
+  })
+})
+
+$(function() {
+  $(".cup2").click(function () {
+    $(".fourth").show(".fourth", "fadeInDown")
+  })
+})
+
+$(function() {
+  $(".cup2").click(function () {
+    $(".fifth").show(".fifth", "fadeInDown")
+  })
+})
