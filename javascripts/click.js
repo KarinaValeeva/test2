eda57f3840c9bfa298a7f34bd1334ad17c360b6b
Karina <Karina@MacBook-Pro-Karina.local>
2017-02-19 18:42:40 +0300

    19.02.17
    Починила скрипт, браузер больше не выдает ошибок

--- javascripts/click.js
+++ javascripts/click.js
@@ -1,50 +1,25 @@
 $(function() {
   $(".cup").click(function () {
-    $(".first").show("figure", 'fadeInDown')
+    $(".first").fadeIn("Down")
   });
 });
 
-
-
 $(function() {
   $(".cup1").click(function () {
-    $(".second").show(".second", "fadeInDown")
+    $(".cup1 figure:hidden:last").fadeIn("Down")
   })
 })
 
-$(function() {
-  $(".cup1").click(function () {
-    $(".third").show(".third", "fadeInDown")
-  })
-})
 
 $(function() {
   $(".cup2").click(function () {
-    $(".fourth").show(".fourth", "fadeInDown")
-  })
-})
-
-$(function() {
-  $(".cup2").click(function () {
-    $(".fifth").show(".fifth", "fadeInDown")
+    $(".cup2 figure:hidden:last").fadeIn("Down")
   })
 })
 
 
 $(function() {
   $(".cup3").click(function () {
-    $(".six").show(".six", "fadeInDown")
-  })
-})
-
-$(function() {
-  $(".cup3").click(function () {
-    $(".seven").show(".seven", "fadeInDown")
-  })
-})
-
-$(function() {
-  $(".cup3").click(function () {
-    $(".eight").show(".eight", "fadeInDown")
+    $(".cup3 figure:hidden:last").fadeIn("Down")
   })
 })
