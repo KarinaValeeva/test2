466c0c3f19fb15c514185e96072d458ed7560781
Karina <Karina@MacBook-Pro-Karina.local>
2017-02-20 21:20:08 +0300

    20.02.17
    Доделала чашечки

--- javascripts/click.js
+++ javascripts/click.js
@@ -1,8 +1,8 @@
 $(function() {
   $(".cup").click(function () {
     $(".first").fadeIn("Down")
-  });
-});
+  })
+})
 
 $(function() {
   $(".cup1").click(function () {
@@ -21,5 +21,35 @@ $(function() {
 $(function() {
   $(".cup3").click(function () {
     $(".cup3 figure:hidden:last").fadeIn("Down")
+  })
+})
+
+$(function() {
+  $(".cup4").click(function () {
+    $(".cup4 figure:hidden:last").fadeIn("Down")
+  })
+})
+
+$(function() {
+  $(".cup5").click(function () {
+    $(".cup5 figure:hidden:last").fadeIn("Down")
+  })
+})
+
+$(function() {
+  $(".cup6").click(function () {
+    $(".cup6 figure:hidden:last").fadeIn("Down")
+  })
+})
+
+$(function() {
+  $(".cup7").click(function () {
+    $(".cup7 figure:hidden:last").fadeIn("Down")
+  })
+})
+
+$(function() {
+  $(".cup8").click(function () {
+    $(".cup8 figure:hidden:last").fadeIn("Down")
   })
 })
